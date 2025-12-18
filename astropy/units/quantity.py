fix(units): preserve inexact dtype when constructing quantities

astropy/units/quantity.py
@@ -296,8 +296,10 @@ class Quantity(np.ndarray, metaclass=InheritDocstrings):
                 if not copy:
                     return value
 
-                if not (np.can_cast(np.float32, value.dtype) or
-                        value.dtype.fields):
+                if not (
+                    np.issubdtype(value.dtype, np.inexact)
+                    or value.dtype.fields
+                ):
                     dtype = float
 
             return np.array(value, dtype=dtype, copy=copy, order=order,
@@ -377,9 +379,13 @@ class Quantity(np.ndarray, metaclass=InheritDocstrings):
                             "Numpy numeric type.")
 
         # by default, cast any integer, boolean, etc., to float
-        if dtype is None and (not (np.can_cast(np.float32, value.dtype)
-                                   or value.dtype.fields)
-                              or value.dtype.kind == 'O'):
+        if dtype is None and (
+            not (
+                np.issubdtype(value.dtype, np.inexact)
+                or value.dtype.fields
+            )
+            or value.dtype.kind == 'O'
+        ):
             value = value.astype(float)
 
         value = value.view(cls)